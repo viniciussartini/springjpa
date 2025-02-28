dee3e96624803e8c1a44ec89a2e6616a7ad69e10
Vinicius Sartini <sartini.vinicius@outlook.com>
2025-02-28 10:39:10 -0300

    Criação da entidade Payment, implementação de subtotal e total em Order

--- src/main/java/br/com/example/springjpa/entities/Order.java
+++ src/main/java/br/com/example/springjpa/entities/Order.java
@@ -99,6 +99,14 @@ public class Order implements Serializable{
         this.payment = payment;
     }
 
+    public Double getTotal() {
+        double sum = 0.0;
+        for(OrderItem item : items) {
+            sum += item.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
